74ba621acce339ea2b4341fc0c70026db549ddeb
BoyWithSilverWings <agney@outlook.in>
2019-01-02 01:33:23 +0530

    add sass package

---
+++ packages/index.js
@@ -0,0 +1,27 @@
+module.exports = (env) => env === "dev" ? ({
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  }
+}) : ({
+  module: {
+    loaders: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  }
+})
