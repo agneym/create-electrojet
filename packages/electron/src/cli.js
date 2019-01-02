69f81c1910899f450e36581a56a92397127cb16b
BoyWithSilverWings <agney@outlook.in>
2019-01-03 01:06:26 +0530

    electron - remove unnecessary files

---
+++ packages/electron/src/cli.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+const meow = require('meow');
+
+const start = require('../src/commands/start');
+const build = require('../src/commands/build');
+const validateCommand = require('./extensions/validateCommand');
+
+function run() {
+  const cli = meow(
+    `
+    Usage
+      $ electrojet-electron <input>
+    
+    Options
+      --port, -p Port
+ 
+    Examples
+      Start the script in development mode.
+      $ electrojet-electron start --port=4567
+
+      Build the app into build targets
+      $ electrojet-electron build
+  `,
+    {
+      flags: {
+        port: {
+          type: 'number',
+          alias: 'p',
+          default: 4567,
+        }
+      }
+    }
+  );
+
+  const command = cli.input[0];
+
+  if (validateCommand(cli, command)) {
+    switch (command) {
+      case 'start':
+        start(cli);
+        break;
+      case 'build':
+        build(cli);
+        break;
+    }
+  }
+}
+
+module.exports = { run };
---
+++ packages/electron/src/extensions/getPort.js
@@ -0,0 +1,23 @@
+const detect = require("detect-port")
+const { prompt } = require("enquirer")
+
+async function getPort(userPort) {
+  const _port = await detect(userPort)
+  if(_port === userPort) {
+    return userPort;
+  } else {
+    const answer = await prompt({
+      type: 'confirm',
+      name: 'port',
+      message: `Seems like ${userPort} is being used by another application. \nWould you like to try ${_port}?`, 
+    });
+    if (!answer.port) {
+      console.warn("\nConsider rerunning the script with --port flag");
+      process.exit();
+    } else {
+      return Promise.resolve(_port);
+    }
+  }
+}
+
+module.exports = getPort;
