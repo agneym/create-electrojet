1c2681ad4d1c69d79cbff0fa1e553fff0eab6343
BoyWithSilverWings <agney@outlook.in>
2019-01-06 21:03:57 +0530

    export config option

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,7 +1,9 @@
 const start = require('./src/commands/start');
 const build = require('./src/commands/build');
+const getConfig = require('./src/extensions/get-config');
 
 module.exports = {
   start,
   build,
+  getConfig,
 };
