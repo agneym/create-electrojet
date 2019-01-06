4e99af5354cb77ade6728d2fc1e975b56c1e5c45
BoyWithSilverWings <agney@outlook.in>
2019-01-06 22:34:14 +0530

    add phaser ce to the examples

--- create-electrojet/src/utils/constants.js
+++ create-electrojet/src/utils/constants.js
@@ -3,4 +3,5 @@ exports.PKG_NAME = "electrojet";
 exports.DEFAULT_PACKAGES = {
   electron: "BoyWithSilverWings/electrojet-default",
   carlo: "BoyWithSilverWings/electrojet-default-carlo",
+  'phaser-ce': 'BoyWithSilverWings/electrojet-phaser-ce',
 };
