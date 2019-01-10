8af3d841185669bd188ff4eb5978ec7e7f8feebd
BoyWithSilverWings <agney@outlook.in>
2019-01-11 02:11:41 +0530

    Add strategy decision for plugin authors

--- packages/core/src/extensions/merge-config.js
+++ packages/core/src/extensions/merge-config.js
@@ -1,6 +1,22 @@
 const webpackMerge = require('webpack-merge');
 
 /**
+ * Derives the default strategy to be used for webpack merge
+ * @param {object | array} result - Result of resolving a plugin
+ * @returns {object}
+ */
+function getStrategy(result) {
+  const defaultStrategy = {
+    plugins: 'append',
+  }
+  if(!Array.isArray(result)) {
+    return defaultStrategy;
+  } else {
+    return result[1];
+  }
+}
+
+/**
  * Merges an array of webpack configurations
  * @param {string} env - Current environment
  * @param {Array} configs - Configuration Array to be merged
@@ -10,8 +26,13 @@ const webpackMerge = require('webpack-merge');
 function mergeConfig(env, configs, initial) {
   const cloneDeep = require("lodash.clonedeep");
   return configs.reduce((acc, configObj) => {
+
     const { resolve, options } = configObj;
-    return webpackMerge.smart(acc, resolve(env, cloneDeep(acc), options));
+    const result = resolve(env, cloneDeep(acc), options);
+    
+    return webpackMerge.smartStrategy(
+      getStrategy(result)
+    )(acc, result[0] || result);
   }, initial);
 }
 
