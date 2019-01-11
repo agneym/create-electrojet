d83fb124d0ea15edaebc14de9ddd39fb53f981dc
BoyWithSilverWings <agney@outlook.in>
2019-01-11 21:43:05 +0530

    Correct export of config function

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,6 +1,6 @@
 const start = require('./src/commands/start');
 const build = require('./src/commands/build');
-const getConfig = require('./src/extensions/get-config');
+const { getConfig } = require('./src/extensions/get-config');
 
 module.exports = {
   start,
