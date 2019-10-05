bffa3a0b5d384034b60eeaa64480f0cd2faec8b0
Agney Menon <agney@outlook.in>
2019-10-05 23:26:05 +0530

    remove deprecated options

--- packages/svelte/src/extensions/config.js
+++ packages/svelte/src/extensions/config.js
@@ -5,37 +5,38 @@ module.exports = {
   default: {
     plugins: [],
   },
-  webpack: (env, context) => [{
-    devServer: {
-      open: true,
+  webpack: (env, context) => [
+    {
+      devServer: {
+        open: true,
+      },
+      resolve: {
+        mainFields: ["svelte", "browser", "module", "main"],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(html|svelte)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "svelte-loader",
+              options: {
+                emitCss: true,
+                hotReload: env === "dev",
+              },
+            },
+          },
+        ],
+      },
+      plugins: [
+        ...context.plugins.slice(1),
+        new HtmlWebpackPlugin({
+          template: path.resolve(process.cwd(), "src/index.ejs"), // This wouldn't work on a normal HTML file
+        }),
+      ],
     },
-    resolve: {
-      mainFields: ['svelte', 'browser', 'module', 'main']
+    {
+      plugins: "replace",
     },
-    module: {
-      rules: [
-        {
-          test: /\.(html|svelte)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'svelte-loader',
-            options: {
-              skipIntroByDefault: true,
-              nestedTransitions: true,
-              emitCss: true,
-              hotReload: env === "dev",
-            }
-          }
-        },
-      ]
-    },
-    plugins: [
-      ...context.plugins.slice(1),
-      new HtmlWebpackPlugin({
-        template: path.resolve(process.cwd(), 'src/index.ejs'), // This wouldn't work on a normal HTML file
-      }),
-    ]
-  }, {
-    plugins: 'replace',
-  }],
+  ],
 };
